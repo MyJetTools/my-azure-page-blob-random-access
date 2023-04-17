0e98a6912550a036c3f7043af8d0cf8a4e4a989a
amigin <amigin@gmail.com>
2023-04-17 17:55:46 +0300

    Added Abstractions

--- src/page_blob_random_access.rs
+++ src/page_blob_random_access.rs
@@ -1,15 +1,13 @@
 use my_azure_storage_sdk::{
-    page_blob::{consts::BLOB_PAGE_SIZE, AzurePageBlobStorage, AzurePageBlobStorageExts},
+    page_blob::{AzurePageBlobStorage, PageBlobAbstractions},
     AzureStorageError,
 };
+use tokio::sync::Mutex;
 
-use crate::{PageBlobRandomAccessError, RandomAccessPageOffsets, ReadChunk};
+use crate::{PageBlobRandomAccessError, PageBlobRandomAccessInner, ReadChunk};
 
 pub struct PageBlobRandomAccess {
-    pub page_blob: AzurePageBlobStorage,
-    page_blob_size: Option<usize>,
-    auto_resize_on_write: bool,
-    max_pages_to_write_per_request: usize,
+    inner: Mutex<PageBlobRandomAccessInner>,
 }
 
 impl PageBlobRandomAccess {
@@ -19,135 +17,64 @@ impl PageBlobRandomAccess {
         max_pages_to_write_per_request: usize,
     ) -> Self {
         Self {
-            page_blob,
-            page_blob_size: None,
-            auto_resize_on_write,
-            max_pages_to_write_per_request,
+            inner: Mutex::new(PageBlobRandomAccessInner::new(
+                page_blob,
+                auto_resize_on_write,
+                max_pages_to_write_per_request,
+            )),
         }
     }
 
-    pub async fn get_blob_size(&mut self) -> Result<usize, AzureStorageError> {
-        if self.page_blob_size.is_some() {
-            return Ok(self.page_blob_size.unwrap());
-        }
-
-        let props = self.page_blob.get_blob_properties().await?;
-
-        self.page_blob_size = Some(props.blob_size);
-
-        return Ok(props.blob_size);
-    }
-
-    async fn resize(&mut self, pages_amount: usize) -> Result<(), AzureStorageError> {
-        self.page_blob.resize(pages_amount).await?;
-
-        self.page_blob_size = Some(pages_amount * BLOB_PAGE_SIZE);
-
-        Ok(())
+    pub async fn get_blob_size(&self) -> Result<usize, AzureStorageError> {
+        let mut write_access = self.inner.lock().await;
+        write_access.get_blob_size().await
     }
 
     pub async fn read(
-        &mut self,
+        &self,
         start_pos: usize,
         size: usize,
     ) -> Result<ReadChunk, PageBlobRandomAccessError> {
-        let available_pages_amount = self.get_blob_size().await? / BLOB_PAGE_SIZE;
-
-        let offsets = RandomAccessPageOffsets::new(start_pos, size);
-
-        let required_pages_amount = offsets.get_required_blob_pages_amount();
-
-        if required_pages_amount > available_pages_amount {
-            let msg = format!(
-                "To perform read operation required blob pages amount is {}. Blob has pages: {}",
-                required_pages_amount, available_pages_amount
-            );
-            return Err(PageBlobRandomAccessError::IndexRangeViolation(msg));
-        }
-
-        let result = self
-            .page_blob
-            .get_pages(offsets.start_page_no, offsets.total_pages_amount_to_read)
-            .await?;
-
-        Ok(ReadChunk::new(offsets.offset_in_page, size, result))
+        let mut write_access = self.inner.lock().await;
+        write_access.read(start_pos, size).await
     }
 
     pub async fn write(
-        &mut self,
+        &self,
         start_pos: usize,
         payload: &[u8],
     ) -> Result<(), PageBlobRandomAccessError> {
-        let available_pages_amount = self.get_blob_size().await? / BLOB_PAGE_SIZE;
-
-        let offsets = RandomAccessPageOffsets::new(start_pos, payload.len());
-
-        let required_pages_amount = offsets.get_required_blob_pages_amount();
-
-        if available_pages_amount < required_pages_amount {
-            if self.auto_resize_on_write {
-                self.resize(required_pages_amount).await?;
-            }
-        }
-
-        let mut payload_from_blob = self
-            .page_blob
-            .get_pages(offsets.start_page_no, offsets.total_pages_amount_to_read)
-            .await?;
-
-        let payload_to_copy =
-            &mut payload_from_blob[offsets.offset_in_page..offsets.offset_in_page + payload.len()];
-
-        payload_to_copy.copy_from_slice(payload);
-
-        self.page_blob
-            .save_pages_ext(
-                offsets.start_page_no,
-                payload_from_blob,
-                self.max_pages_to_write_per_request,
-            )
-            .await?;
-
-        Ok(())
+        let mut write_access = self.inner.lock().await;
+        write_access.write(start_pos, payload).await
     }
 
-    pub async fn create_new(&mut self, pages: usize) -> Result<(), AzureStorageError> {
-        self.page_blob.create_if_not_exists(pages).await?;
+    pub async fn create_blob_if_not_exists(
+        &self,
+        init_pages_amount: usize,
+    ) -> Result<usize, AzureStorageError> {
+        let mut write_access = self.inner.lock().await;
+        write_access.create_new(init_pages_amount).await
+    }
 
-        Ok(())
+    pub async fn create_container_if_not_exists(&self) -> Result<(), AzureStorageError> {
+        let write_access: tokio::sync::MutexGuard<PageBlobRandomAccessInner> =
+            self.inner.lock().await;
+        write_access
+            .page_blob
+            .create_container_if_not_exists()
+            .await
     }
 }
 
-#[cfg(test)]
-mod test {
-
-    use std::sync::Arc;
-
-    use my_azure_storage_sdk::AzureStorageConnection;
-
-    use super::*;
-
-    #[tokio::test]
-    async fn test_basic_cases() {
-        let connection = AzureStorageConnection::new_in_memory();
-
-        let page_blob = AzurePageBlobStorage::new(Arc::new(connection), "Test", "Test").await;
-        page_blob.create_container_if_not_exist().await.unwrap();
-        page_blob.create_if_not_exists(0).await.unwrap();
-
-        let mut random_access = PageBlobRandomAccess::new(page_blob, true, 10);
-
-        random_access
-            .write(3, vec![1u8, 2u8, 3u8].as_slice())
-            .await
-            .unwrap();
-
-        let result = random_access.page_blob.download().await.unwrap();
-
-        assert_eq!(vec![0u8, 0u8, 0u8, 1u8, 2u8, 3u8, 0u8, 0u8], result[0..8]);
-
-        let result = random_access.read(3, 4).await.unwrap();
-
-        assert_eq!(vec![1u8, 2u8, 3u8, 0u8], result.as_slice());
+#[async_trait::async_trait]
+impl PageBlobAbstractions for PageBlobRandomAccess {
+    async fn create_container_if_not_exists(&self) -> Result<(), AzureStorageError> {
+        self.create_container_if_not_exists().await
+    }
+    async fn create_blob_if_not_exists(
+        &self,
+        init_pages_amounts: usize,
+    ) -> Result<usize, AzureStorageError> {
+        self.create_blob_if_not_exists(init_pages_amounts).await
     }
 }
