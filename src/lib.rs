1ddf28046b3542c96b38260b623e2dc4046013e3
amigin <amigin@gmail.com>
2024-11-20 11:14:11 +0200

    rust-extensions:0.1.5

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,6 @@ mod page_blob_random_access;
 mod random_access_page_offsets;
 mod read_chunk;
 pub use errors::PageBlobRandomAccessError;
-pub use page_blob_random_access::PageBlobRandomAccess;
 pub use page_blob_random_access::*;
 pub use random_access_page_offsets::*;
 pub use read_chunk::*;
